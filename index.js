d5a2122ed3e8bbd5fa031445b2159eee4b46c553
faithhopeandvanity <faithhopeandvanity@gmail.com>
2024-02-02 18:41:04 +0000

    first criteria

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-//packages
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+// external packages
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [{
+  type: 'input',
+  name: 'title',
+  message: 'What is the name of your project?  (Required)',
+  validate: nameInput => {
+      if (nameInput) {
+          return true;
+      } else {
+          console.log('Please tell us your project name!');
+          return false;
+      }
+  }
+},
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
